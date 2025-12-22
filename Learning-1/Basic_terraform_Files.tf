4d7418b294caf5c5993ce367e40beb15b81ed72d
Phaneendra kavala <phani.kavala@gamil.com>
2025-12-23 00:46:51 +0530

    fourth commit

--- Learning-1/Basic_terraform_Files.tf
+++ Learning-1/Basic_terraform_Files.tf
@@ -3,6 +3,15 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami                     = "ami-02b8269d5e85954ef"
+  ami                     = "ami-00ca570c1b6d79f36"
   instance_type           = "t3.micro"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
