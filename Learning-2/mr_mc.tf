cc4865c8d1f21b95cb378356f30f54737076e323
Phaneendra kavala <phani.kavala@gamil.com>
2025-12-31 09:53:59 +0530

    2nd commit

--- Learning-2/mr_mc.tf
+++ Learning-2/mr_mc.tf
@@ -10,12 +10,12 @@ provider "aws" {
 
 resource "aws_instance" "mr_mc" {
     ami = "ami-00d8fc944fb171e29"
-    instance_type = "t3-micro"
+    instance_type = "t3.micro"
     provider = "aws.ap-southeast-1"
 }
 
 resource "aws_instance" "mr_mc_2" {
     ami = "ami-0b8d527345fdace59"
-    instance_type = "t3-micro"
+    instance_type = "t3.micro"
     provider = "aws.ap-southeast-2"
 }
