006a123a4777586455cd3f31227b786bb1c1d542
Phaneendra kavala <phani.kavala@gamil.com>
2026-01-14 21:52:37 +0530

    5th commit remote backends

--- Learning-4/L4_main.tf
+++ Learning-4/L4_main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "MyFirstEC2Instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  key_name      = var.key_pair
+  key_name = aws_key_pair.ec2_key.key_name
   subnet_id     = var.subnet_id
 
   vpc_security_group_ids = var.vpc_security_group_ids
